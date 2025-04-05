7d0145277c72146ac0931fe58169c47864f38b2a
Sonson <113127683+sonsonmagro@users.noreply.github.com>
2025-04-05 17:37:12 +0200

    Update main.lua

--- rasial/main.lua
+++ rasial/main.lua
@@ -1,9 +1,20 @@
----@version 1.0.0
-local version = "1.0.0"
+---@version 1.0.1
+local version = "1.0.1"
 local API = require("api")
 
 --[[
+    quick fix for debug
+        - utils debug vs config debug
+    fix for mentsions in discord webhook
+    added extra wait tick before target cycling
+    added more room for config execute
+    fix kill logging (fastest/slowest kills)
+    fiexed not equipping lotd
 
+
+    TODO: 
+    confirm drop alerts are fixed
+    seedicide and spring cleaner support
 ]]
 
 local PlayerManager     = require("core.player_manager")        -- file saved in Lua_Scripts\core
@@ -16,14 +27,13 @@ local scriptStartTime = os.time()
 local playerManager, prayerFlicker = PlayerManager.new(Config.playerManager), Config.Instances.prayerFlicker
 
 --#region tracking table generation
-local function tracking(...)
-    local args = { ... }
+local function tracking()
     local metrics = {
         { "Sonson's Rasial", "Version: " .. version },
         { "", "" },
         { "Metrics:",API.ScriptRuntimeString() },
-        not Config.UserInput.debug and {"- Status: ", playerManager.state.status},
-        not Config.UserInput.debug and {"- Location: ", playerManager.state.location},
+        {"- Status: ", playerManager.state.status} or {},
+        {"- Location: ", playerManager.state.location} or {},
         { "- Total Kills (/hr)", Config.Variables.killCount .. string.format(" (%s)", Utils.valuePerHour(Config.Variables.killCount, scriptStartTime)) },
         { "- Total Rares (/hr)", #Config.Data.lootedUniques .. string.format(" (%s)", Utils.valuePerHour(#Config.Data.lootedUniques, scriptStartTime)) },
         { "", "" },
@@ -32,9 +42,14 @@ local function tracking(...)
         { "- Slowest Kill:", Utils.getKillStats(Config.TrackedKills).slowestKillDuration },
         { "- Average Kill Time:", Utils.getKillStats(Config.TrackedKills).averageKillDuration }
     }
-    local debuggingTable = {
-        { "Debugging:" },
-        --{ "- Spellbook",  Utils.getSpellbook() },
+
+    -- change this depending on what you want to track whilst debugging
+    local trackedDebuggingTables = {
+        playerManager:stateTracking(),
+        playerManager:managementTracking(),
+        playerManager:foodItemsTracking(),
+        playerManager:prayerItemsTracking(),
+        playerManager:managedBuffsTracking()
     }
 
     if #Config.Data.lootedUniques > 0 then
@@ -44,14 +59,20 @@ local function tracking(...)
         Utils.tableConcat(metrics, Config.Data.lootedUniques)
     end
 
-    if Config.UserInput.debug then 
-    for _, table in pairs(args) do
+    --view kill details
+    if Utils.debug and #Config.TrackedKills > 0 then
+        Utils.tableConcat(metrics, { { "", "" } })
+        Utils.tableConcat(metrics, { { "Kill Details:", "" } })
+        for i, killData in pairs(Config.TrackedKills) do
+            Utils.tableConcat(metrics, { { string.format("- [%s] %s", i, killData.runtime), killData.fightDuration } })
+        end
+    end
+
+    if Utils.debug then 
+        for _, table in pairs(trackedDebuggingTables) do
             Utils.tableConcat(metrics, { { "", "" } })
             Utils.tableConcat(metrics, table)
         end
-
-        Utils.tableConcat(metrics, { { "", "" } })
-        Utils.tableConcat(metrics, debuggingTable)
     end
 
     API.DrawTable(metrics) -- draw table pls
@@ -63,17 +84,7 @@ while API.Read_LoopyLoop() do
     playerManager:update()
     if playerManager.state.location ~= "Rasial's Citadel (Boss Room)" then prayerFlicker:deactivatePrayer() end
     -- completely optional stats & metrics
-    tracking(
-        -- player manager tracking
-        Config.UserInput.debug and playerManager:stateTracking(),
-        Config.UserInput.debug and playerManager:managementTracking(),
-        Config.UserInput.debug and playerManager:foodItemsTracking(),
-        Config.UserInput.debug and playerManager:prayerItemsTracking(),
-        Config.UserInput.debug and playerManager:activeBuffsTracking(),
-        Config.UserInput.debug and playerManager:managedBuffsTracking(),
-        --prayer flicker tracking
-        Config.UserInput.debug and prayerFlicker:tracking()
-    )
+    tracking()
     -- very short zzz
     API.RandomSleep2(10, 10, 10)
 end
